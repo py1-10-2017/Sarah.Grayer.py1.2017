3570579d1b18cce4824d1e5e491cd61b28785152
SarahGrayer <slawler689@gmail.com>
2017-10-09 21:11:52 -0500

    update landing_page

--- Flask Fundamentals/Landing_Page/Landing_Page.py
+++ Flask Fundamentals/Landing_Page/Landing_Page.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/ninjas')
+def ninja():
     return render_template("ninjas.html")
+
+@app.route('/dojos')
+def dojos():
+    return render_template("dojos.html")
+
 app.run(debug=True)
