ea6af43b09434177fe6afb04dfb48eb77ab0d5ed
SarahGrayer <slawler689@gmail.com>
2017-10-18 21:07:48 -0500

    add SQL folder

--- Flask Fundamentals/Registration From/registration.py
+++ Flask Fundamentals/Registration From/registration.py
@@ -12,48 +12,38 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     errors = False
-    print request.form
 
     if len(request.form['firstname']) <1:
         flash('First name cannot be empty')
         errors = True
-        #return redirect('/')
 
     elif not request.form['firstname'].isalpha():
         flash('First name cannot contain numbers or special characters')
         errors = True
-        #return redirect('/')
 
     if len(request.form['lastname']) < 1:
         flash('Last name cannot be empty')
         errors = True
-        #return redirect('/')
     elif not request.form['lastname'].isalpha():
         flash('Last name cannot contain numbers or special characters')
         errors = True
-        #return redirect('/')
 
     if len(request.form['email']) < 1:
         flash('Email cannot be empty')
         errors = True
-        #return redirect('/')
     elif not EMAIL_REGEX.match(request.form['email']):
         flash('Invalid email')
         errors = True
-        #return redirect('/')
 
     if len(request.form['pass']) < 8:
         flash('Password must be 8 or more characters')
         errors = True
-        #return redirect('/')
     elif not PASSWORD_REGEX.match(request.form['pass']):
         flash('Password must contain at least one uppercase letter and at least one number')
         errors = True
-        #return redirect('/')
     elif request.form['pass'] != request.form['passconfirm']:
         flash('Passwords must match')
         errors = True
-        #return redirect('/')
 
     if errors == False:
         flash('Thank you for submitting your information!')
