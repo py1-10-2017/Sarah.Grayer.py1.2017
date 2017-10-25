64307e6e0809cf3c858f4e489a94b4c68d3159a2
SarahGrayer <slawler689@gmail.com>
2017-10-25 17:05:33 -0500

    add email

---
+++ SQL/Flask_MySQL/email_validation/server.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, redirect, session, flash, request
+from mysqlconnection import MySQLConnector
+
+app = Flask(__name__)
+app.secret_key = "secret_key"
+
+import re
+
+EMAIL_REGEX = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+mysql = MySQLConnector(app, 'email')
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+	email = request.form['email']
+	print email
+
+	if not EMAIL_REGEX.match(email):
+		flash('Invalid email.')
+		print "Email fail"
+		return redirect('/')
+	else:
+		query = "INSERT INTO email(email, created_at) VALUES(:email, NOW())"
+		data = {
+			'email' : request.form['email']
+		}
+		session['email'] = email
+		mysql.query_db(query, data)
+		return redirect('/success')
+
+@app.route('/success')
+def success():
+    query = "SELECT * FROM email" # define your query
+    emails = mysql.query_db(query) # run query with query_db()
+    return render_template('success.html', all_emails=emails, email=session['email']) # pass data to our template
+
+@app.route('/home')
+def home():
+    	session.pop('email')
+	return redirect('/')
+
+@app.route('/remove_email/<email_id>')
+def delete(email_id):
+    query = "DELETE FROM email WHERE id = :id"
+    data = {'id': email_id}
+    mysql.query_db(query, data)
+    return redirect('/success')
+
+app.run(debug=True)
---
+++ SQL/Flask_MySQL/friends/server.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+mysql = MySQLConnector(app,'friendsdb')
+
+@app.route('/')
+def index():
+    query = "SELECT * FROM friends" #define query
+    friends = mysql.query_db(query) #run query with query_db()
+    print friends
+    return render_template('index.html', all_friends=friends) #pass data to our template
+
+@app.route('/friends', methods=['POST'])
+#this route handles adding a friend and then redirects to base route which already
+#retrieves all friends and displays them.
+def create():
+    # add a friend to the database!
+    #print request.form['first_name']
+    #print request.form['last_name']
+    #print request.form['occupation']
+    #return redirect('/')
+
+    #Write query as a string. Notice how we have multiple valus we want to
+    #insert into our query.
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, :NOW(), :NOW())"
+    #We'll then create a dictionary of data from the POST data received.
+    data = {
+            'first_name': request.form['first_name'],
+            'last_name': request.form['last_name'],
+            'occupation': request.form['occupation']
+            }
+    #Run query, with dictionary values injected into the query.
+    mysql.query_db(query, data)
+    return redirect('/')
+
+#@app.route('/update_friend/<friend_id>', methods=['POST'])
+#def update(friend_id):
+#    query = "UPDATE friends"
+#    SET first_name = :first_name, last_name = :last_name, occupation = :occupation
+#    WHERE id = :id
+#    data = {
+#             'first_name': request.form['first_name'],
+#             'last_name':  request.form['last_name'],
+#             'occupation': request.form['occupation'],
+#             'id': friend_id
+#           }
+#    mysql.query_db(query, data)
+#    return redirect('/')
+
+@app.route('/remove_friend/<friend_id>', methods=['POST'])
+def delete(friend_id):
+    query = "DELETE FROM friends WHERE id = :id"
+    data = {'id': friend_id}
+    mysql.query_db(query, data)
+    return redirect('/')
+
+@app.route('/friends/<friend_id>')
+def show(friend_id):
+    #write query to select specific user by id. At every point where we
+    #want to insert data, we write ":" and variable name.
+    Query = "SELECT * FROM friends WHERE id = :specific_id"
+    #Then define a dictionary with key that matches :variable_name in query.
+    data = {'specific_id':8}
+    #Run query with inserted data
+    friends = mysql.query_db(query, data)
+    #Friends should be a lsit wtih a single object, so we pass the value
+    # at [0] to our template under alias one_friend.
+    return render_template('index.html', one_friend = friends[0])
+
+app.run(debug=True)
