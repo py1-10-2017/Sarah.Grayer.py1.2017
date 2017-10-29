96594babf27a5c03c46852691a96fded0b3ffd2e
SarahGrayer <slawler689@gmail.com>
2017-10-28 23:39:48 -0500

    adding full-friends

--- Flask_MySQL/full_friends/server.py
+++ Flask_MySQL/full_friends/server.py
@@ -11,22 +11,46 @@ mysql = MySQLConnector(app, 'full_friends_db')
 
 @app.route('/')
 def index(): #Display all of the friends on the index.html page
-    query = "SELECT * FROM friends"
+    query = "SELECT * FROM friends" #defines what the query below will do
     friends = mysql.query_db(query)
-    return render_template('index.html')
+    return render_template('index.html', all_friends = friends) #passes data to template
 
+@app.route('/addfriends', methods=['POST'])
+def create(): #Handle the add friend form submit and create the friend in the DB
+    query= "INSERT INTO friends (first_name, last_name, email, created_at) VALUES(:first_name, :last_name, :email, NOW())"
+    data = { #Dict of data below from POST data received
+		'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+	}
+    mysql.query_db(query, data)
+    print "friend added"
+    return redirect('/')
 
+@app.route('/edit_friend/<friend_id>', methods=['POST', 'GET'])
+def edit(friend_id): #Display the edit friend page for the particular friend
+    print "edit"
+    return render_template('edit.html')
 
-#@app.route('/friends', methods=["POST"])
-#def create(): #Handle the add friend form submit and create the friend in the DB
-    #return render_template('index.html')
-    #@app.route('/friends/')
-    #def edit(id): #Display the edit friend page for the particular friend
+@app.route('/friend/<friend_id>', methods=['POST', 'GET'])
+def update(friend_id):#Handle the edit friend form submit and update the friend in the DB
+    query = "UPDATE friends SET first_name= :first_name, last_name= :last_name, email= :email WHERE id= :id"
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name':  request.form['last_name'],
+        'email': request.form['email'],
+        'id': id
+        }
+    mysql.query_db(query, data)
+    print "update"
+    return redirect('/')
 
-    #@app.route('/friends/<id>', methods=["POST"])
-    #def update(id): #Handle the edit friend form submit and update the friend in the DB
-
-    #@app.route('/friends/<id>/delete', methods=["POST"])
-    #def destroy(id): #Delete the friend from the DB
+@app.route('/delete_friend/<friend_id>', methods=['POST', 'GET'])
+def destroy(friend_id): #Delete the friend from the DB
+    query = "DELETE FROM friends WHERE id = :id"
+    data = {'id': friend_id}
+    mysql.query_db(query, data)
+    print "delete"
+    return redirect('/')
 
 app.run(debug=True)
