79373af7087a3be8e561fd20e79dffb116da5c4a
SarahGrayer <slawler689@gmail.com>
2017-11-06 17:13:19 -0600

    Commented in django_app urls.py

--- Django/Django Intro/django_app/apps/blogs/urls.py
+++ Django/Django Intro/django_app/apps/blogs/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^new$', views.new),
-    url(r'^create$', views.create),
-    url(r'^(?P<blog_id>\d+)$', views.show),
-    url(r'^(?P<blog_id>\d+)/edit$', views.edit),
-    url(r'^(?P<blog_id>\d+)/delete$', views.destroy)
+    url(r'^$', views.index), #blank, index page
+    url(r'^new$', views.new), #begins & ends w/new, views page & "new" method
+    url(r'^create$', views.create), #begins & ends w/create, views page & "create" method
+    url(r'^(?P<blog_id>\d+)$', views.show), #begins & ends w/a # which is a blog id, passes into "show" method
+    url(r'^(?P<blog_id>\d+)/edit$', views.edit), # of blog id / edit, # passed to "edit"
+    url(r'^(?P<blog_id>\d+)/delete$', views.destroy) # of blog id / delete
 ]
