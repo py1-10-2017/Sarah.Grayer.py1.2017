83f6831fb5996fab157fa2ece8c152b1dc7dd038
SarahGrayer <slawler689@gmail.com>
2017-11-07 21:52:14 -0600

    adding random word generator

---
+++ Django/Django Intro/RandomWordGenerator/apps/random_word/views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import random
+import string
+from django.shortcuts import render, redirect
+
+def random_word(n):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
+
+# views
+def index(request):
+    try:
+        request.session['tries']
+    except KeyError:
+        request.session['tries'] = 0
+
+    return render(request, "random_word/index.html")
+
+def generate(request):
+    request.session['tries'] += 1
+    request.session['word'] = random_word(10)
+    return redirect('/')
+
+def reset(request):
+    del request.session['tries']
+    del request.session['word']
+    return redirect('/')
